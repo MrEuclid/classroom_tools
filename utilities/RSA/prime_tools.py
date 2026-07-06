93164e5e6594fbc3e40a9eb29208a17142537988
John Thompson <john@teacherjohn.org>
2026-07-06 17:42:49 +0700

    prime check

--- utilities/RSA/prime_tools.py
+++ utilities/RSA/prime_tools.py
@@ -114,6 +114,18 @@ def get_prime_factors(n):
         factors.add(n)
         
     return factors
+def is_prime(n):
+    """Returns True if n is a prime number, False otherwise."""
+    if n < 2: return False
+    if n in (2, 3): return True
+    if n % 2 == 0 or n % 3 == 0: return False
+    
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
 def find_primitive_roots(p, max_results=10):
     """Finds up to 'max_results' primitive roots modulo a prime p."""
